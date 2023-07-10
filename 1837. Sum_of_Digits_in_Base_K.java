b6872441ce2ce7eb16ff11b8085ce93b46550bd9
sakthiram2004 <138390991+sakthiram2004@users.noreply.github.com>
2023-07-10 14:54:02 +0530

    Add files via upload

---
+++ 1837. Sum_of_Digits_in_Base_K.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumBase(int n, int k) {
+        int s=0;
+        while(n!=0)
+        {
+            int r=n%k;
+            s=s+r;
+            n=n/k;
+
+        }
+        return s;
+        
+    }
+}
